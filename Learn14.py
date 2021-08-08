879ac78d529fca20cc6edeb8d09fb2946239fd0e
syedraza <majidraza@hotmail.com>
2021-08-08 00:04:23 -0400

    new file for logs

---
+++ Learn14.py
@@ -0,0 +1,5 @@
+# Given an array of words, return all strings in words that are a substring of another word.
+# Note: The order in which you return the substrings does not matter.
+# Ex: Given the following words…
+# words = ["abc", "a", "b"], return ["a", "b"].
+
