889dc0e9af52a3ae551d9832f1cf238b9bc9c619
syedraza <majidraza@hotmail.com>
2022-02-11 23:54:09 -0500

    Start coding practice

--- Learn20.py
+++ Learn20.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-#  Given a string, reverse all of its characters and return the resulting string.
-
-# Ex: Given the following strings...
-
-# “Cat”, return “taC”
-# “The Daily Byte”, return "etyB yliaD ehT”
-# “civic”, return “civic”
-
-
-def reverse(sreverse):
-    if len(sreverse) == 1:
-        return sreverse
-    else:
-        return reverse(sreverse[1:]) + sreverse[0]
-    if len(sreverse) == 0:
-        return sreverse
-    else:
-        return reverse(sreverse[1:]) + sreverse[0]
-        
-
-print(reverse("Cat"))
-
-# Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
-
-=======
 def some_func():
     try:
         return "from try func"
@@ -57,4 +31,3 @@ def one_more_func():
 # print(another_func())
 
 print(one_more_func())
->>>>>>> e62631bd950cd92afae07d365aca5d3f77280a92
